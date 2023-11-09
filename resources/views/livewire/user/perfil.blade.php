e431c5c722097bd228841d64d919dbe00f006e1c
paullcv <paul.cruz.4.pc@gmail.com>
2023-11-09 01:02:47 -0400

    Mejora en el aspecto visual

--- resources/views/livewire/user/perfil.blade.php
+++ resources/views/livewire/user/perfil.blade.php
@@ -1,69 +1,35 @@
 <div>
     <!-- Current Profile Photos -->
-    @if (auth()->user()->photo_path1)
-        <div class="mt-2">
-            <img src="{{ asset('storage/' . auth()->user()->photo_path1) }}" alt="Current Profile Photo 1" class="rounded-full h-20 w-20 object-cover">
-        </div>
-    @endif
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+        @foreach (['photo_path1', 'photo_path2', 'photo_path3'] as $attribute)
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-4">
+                @if (auth()->user()->$attribute)
+                    <img src="{{ asset('storage/' . auth()->user()->$attribute) }}" alt="Current Profile Photo" class="rounded-full h-20 w-20 object-cover mx-auto">
+                @else
+                    <span class="text-gray-500">No photo</span>
+                @endif
 
-    @if (auth()->user()->photo_path2)
-        <div class="mt-2">
-            <img src="{{ asset('storage/' . auth()->user()->photo_path2) }}" alt="Current Profile Photo 2" class="rounded-full h-20 w-20 object-cover">
-        </div>
-    @endif
+                <!-- Remove Current Photo Button -->
+                <x-secondary-button type="button" class="mt-2" wire:click="deleteProfilePhoto('{{ $attribute }}')">
+                    {{ __('Remove Photo') }}
+                </x-secondary-button>
+            </div>
+        @endforeach
 
-    @if (auth()->user()->photo_path3)
-        <div class="mt-2">
-            <img src="{{ asset('storage/' . auth()->user()->photo_path3) }}" alt="Current Profile Photo 3" class="rounded-full h-20 w-20 object-cover">
-        </div>
-    @endif
+        <!-- New Profile Photos Inputs -->
+        @foreach (['photo_path1', 'photo_path2', 'photo_path3'] as $attribute)
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-4">
+                <input type="file" wire:model="{{ $attribute }}" class="mt-2">
+                @if ($$attribute)
+                    <x-button wire:click="updateProfilePhoto('{{ $attribute }}')" wire:loading.attr="disabled">
+                        {{ __('Upload Photo') }}
+                    </x-button>
+                    <img src="{{ $$attribute->temporaryUrl() }}" alt="New Photo Preview" class="mt-2">
+                @endif
 
-    <!-- Remove Current Photos Buttons -->
-    <x-secondary-button type="button" class="mt-2" wire:click="deleteProfilePhoto('photo_path1')">
-        {{ __('Remove Photo 1') }}
-    </x-secondary-button>
-    <x-secondary-button type="button" class="mt-2" wire:click="deleteProfilePhoto('photo_path2')">
-        {{ __('Remove Photo 2') }}
-    </x-secondary-button>
-    <x-secondary-button type="button" class="mt-2" wire:click="deleteProfilePhoto('photo_path3')">
-        {{ __('Remove Photo 3') }}
-    </x-secondary-button>
-
-    <!-- New Profile Photos Inputs -->
-    <input type="file" wire:model="photo_path1" class="mt-2">
-    @if ($photo_path1)
-        <x-button wire:click="updateProfilePhoto('photo_path1')" wire:loading.attr="disabled">
-            {{ __('Upload Photo 1') }}
-        </x-button>
-    @endif
-
-    <input type="file" wire:model="photo_path2" class="mt-2">
-    @if ($photo_path2)
-        <x-button wire:click="updateProfilePhoto('photo_path2')" wire:loading.attr="disabled">
-            {{ __('Upload Photo 2') }}
-        </x-button>
-    @endif
-
-    <input type="file" wire:model="photo_path3" class="mt-2">
-    @if ($photo_path3)
-        <x-button wire:click="updateProfilePhoto('photo_path3')" wire:loading.attr="disabled">
-            {{ __('Upload Photo 3') }}
-        </x-button>
-    @endif
-
-    <!-- Previews of the New Photos -->
-    @if ($photo_path1)
-        <img src="{{ $photo_path1->temporaryUrl() }}" alt="New Photo 1 Preview" class="mt-2">
-    @endif
-    @if ($photo_path2)
-        <img src="{{ $photo_path2->temporaryUrl() }}" alt="New Photo 2 Preview" class="mt-2">
-    @endif
-    @if ($photo_path3)
-        <img src="{{ $photo_path3->temporaryUrl() }}" alt="New Photo 3 Preview" class="mt-2">
-    @endif
-
-    <!-- Input Errors -->
-    <x-input-error for="photo_path1" class="mt-2" />
-    <x-input-error for="photo_path2" class="mt-2" />
-    <x-input-error for="photo_path3" class="mt-2" />
+                <!-- Input Errors -->
+                <x-input-error for="{{ $attribute }}" class="mt-2" />
+            </div>
+        @endforeach
+    </div>
 </div>
